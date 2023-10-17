778f96be96127315111ea4b57a8dfbc8beede485
Raymond Shiner <raymondshiner@gmail.com>
2023-10-16 20:32:47 -0700

    Initial commit

---
+++ app1/craco.config.js
@@ -0,0 +1,18 @@
+const { CracoAliasPlugin} = require('react-app-alias');
+
+module.exports = {
+    plugins: [{
+        plugin: require("./craco-plugins/module-federation")
+    },
+        {   plugin: CracoAliasPlugin,
+            options: {
+                source: "tsconfig",
+                // baseUrl SHOULD be specified
+                // plugin does not take it from tsconfig
+                baseUrl: ".",
+                // tsConfigPath should point to the file where "baseUrl" and "paths" are specified
+                tsConfigPath: "./tsconfig.paths.json"
+            }
+        }
+    ]
+};
